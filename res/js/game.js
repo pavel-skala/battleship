bf92e95bb2ab7f5592e97d44dc97d65bf8c9d18d
pavel-skala <pavelskalamobil@gmail.co>
2023-04-11 22:49:40 +0200

    placing ship finished

--- res/js/game.js
+++ res/js/game.js
@@ -4,8 +4,10 @@ const shipsGarageBx = document.getElementsByClassName("shipsGarageBx");
 const saveShipButton = document.getElementById("saveShipButton");
 const buildGarageOverLap = document.getElementById("buildGarageOverLap");
 const saveButtonOverLap = document.getElementById("saveButtonOverLap");
+const rotateButtonOverLap = document.getElementById("rotateButtonOverLap");
 const rotateShipButton = document.getElementById("rotateShipButton");
-const buildSquare = document.getElementsByClassName("buildSquare");
+const buildBoard = document.getElementById("buildBoard");
+// const buildSquare = document.getElementsByClassName("buildSquare");
 const buildShipContinue = document.getElementById("buildShipContinue");
 
 const game = document.getElementById("game");
@@ -39,30 +41,51 @@ let rotateIndex = false;
 let shipFreeSquare = true;
 let savedShipsCount = 0;
 
+let buildSquare = [];
+
 for (let i = 0; i < 100; i++) {
     shipSquareIndex[i] = false;
     myAreaIndex[i] = 0;
+
+    buildSquare[i] = document.createElement("div");
+    buildSquare[i].classList.add("buildSquare");
+    buildBoard.appendChild(buildSquare[i]);
 }
 
 function freeSquareCheck() {
     shipFreeSquare = true;
-        for (
-            let i = shipInGaragePosition[shipPickedIndex];
-            i <
-            shipInGaragePosition[shipPickedIndex] +
-                10 * shipBxinShip[shipPickedIndex];
-            i += 10
-        ) {
-            if (shipSquareIndex[i] === true) {
-                shipFreeSquare = false;
-            }
+    for (
+        let i = shipInGaragePosition[shipPickedIndex];
+        i <
+        shipInGaragePosition[shipPickedIndex] +
+            10 * shipBxinShip[shipPickedIndex];
+        i += 10
+    ) {
+        if (shipSquareIndex[i] === true) {
+            shipFreeSquare = false;
         }
+    }
 
-        if (shipFreeSquare === false) {
-            saveShipButton.style.backgroundColor = "#eb3939";
-        } else {
-            saveShipButton.style.backgroundColor = "#07d307";
-        }
+    if (shipFreeSquare === false) {
+        saveShipButton.style.backgroundColor = "#eb3939";
+    } else {
+        saveShipButton.style.backgroundColor = "#07d307";
+    }
+}
+
+function shipPlacingWithPx() {
+    shipsInGarage[shipPickedIndex].style.left = `${
+        shipInGaragePositionX[shipPickedIndex] * 60 + shipOffSetX
+    }px`;
+    shipsInGarage[shipPickedIndex].style.top = `${
+        shipInGaragePositionY[shipPickedIndex] * 60 + shipOffSetY
+    }px`;
+}
+
+function shipPositionCalc() {
+    shipInGaragePosition[shipPickedIndex] =
+        shipInGaragePositionY[shipPickedIndex] * 10 +
+        shipInGaragePositionX[shipPickedIndex];
 }
 
 [...shipsInGarage].forEach((shipInGarage, shipGarageIndex) => {
@@ -72,30 +95,28 @@ function freeSquareCheck() {
         movingIndex = true;
         shipPickedIndex = shipGarageIndex;
 
-        rotateIndex = false
-        shipOffSetX = 561;
-        shipOffSetY = 91;
+        rotateIndex = false;
 
         shipInGarage.style.position = "absolute";
         shipInGarage.style.zIndex = "1000";
 
         let i = 0;
-        for (i; i < shipGarageIndex.length; i++) {
-            if (shipGarageIndex[i] === false) {
-                shipGarageIndex[i] = true;
+        for (i; i < 100; i++) {
+            if (shipSquareIndex[i] === false) {
                 break;
             }
         }
+
         shipInGaragePosition[shipGarageIndex] = i;
         shipBxinShip[shipGarageIndex] =
             shipInGarage.getElementsByClassName("shipsGarageBx").length;
         console.log(shipBxinShip);
 
-        shipInGaragePositionX[shipGarageIndex] = Math.floor(
+        shipInGaragePositionX[shipGarageIndex] =
+            shipInGaragePosition[shipGarageIndex] % 10;
+        shipInGaragePositionY[shipGarageIndex] = Math.floor(
             shipInGaragePosition[shipGarageIndex] / 10
         );
-        shipInGaragePositionY[shipGarageIndex] =
-            shipInGaragePosition[shipGarageIndex] % 10;
         shipInGarage.style.left = `${
             shipInGaragePositionX[shipGarageIndex] * 60 + shipOffSetX
         }px`;
@@ -108,67 +129,42 @@ function freeSquareCheck() {
         shipInGarage.style.cursor = "default";
         buildGarageOverLap.style.zIndex = "10";
         saveButtonOverLap.style.zIndex = "0";
+        rotateButtonOverLap.style.zIndex = "0";
         shipInGarage.removeEventListener("click", shipInGarageClick);
     }
 });
 
 document.addEventListener("keydown", ({ keyCode }) => {
-    
     if (movingIndex === true) {
         if (keycodes.UP[keyCode]) {
             if (shipInGaragePositionY[shipPickedIndex] > 0) {
                 shipInGaragePositionY[shipPickedIndex]--;
             }
-            shipsInGarage[shipPickedIndex].style.left = `${
-                shipInGaragePositionX[shipPickedIndex] * 60 + shipOffSetX
-            }px`;
-            shipsInGarage[shipPickedIndex].style.top = `${
-                shipInGaragePositionY[shipPickedIndex] * 60 + shipOffSetY
-            }px`;
+            shipPlacingWithPx();
         }
 
         if (keycodes.DOWN[keyCode]) {
             if (rotateIndex === false) {
-                
-            if (
-                shipInGaragePositionY[shipPickedIndex] <=
-                9 - shipBxinShip[shipPickedIndex]
-            ) {
-                shipInGaragePositionY[shipPickedIndex]++;
+                if (
+                    shipInGaragePositionY[shipPickedIndex] <=
+                    9 - shipBxinShip[shipPickedIndex]
+                ) {
+                    shipInGaragePositionY[shipPickedIndex]++;
+                }
+                shipPlacingWithPx();
+            } else {
+                if (shipInGaragePositionY[shipPickedIndex] <= 8) {
+                    shipInGaragePositionY[shipPickedIndex]++;
+                }
+                shipPlacingWithPx();
             }
-            shipsInGarage[shipPickedIndex].style.left = `${
-                shipInGaragePositionX[shipPickedIndex] * 60 + shipOffSetX
-            }px`;
-            shipsInGarage[shipPickedIndex].style.top = `${
-                shipInGaragePositionY[shipPickedIndex] * 60 + shipOffSetY
-            }px`;
-        }
-        else{
-            if (
-                shipInGaragePositionY[shipPickedIndex] <=
-                8
-            ) {
-                shipInGaragePositionY[shipPickedIndex]++;
-            }
-            shipsInGarage[shipPickedIndex].style.left = `${
-                shipInGaragePositionX[shipPickedIndex] * 60 + shipOffSetX
-            }px`;
-            shipsInGarage[shipPickedIndex].style.top = `${
-                shipInGaragePositionY[shipPickedIndex] * 60 + shipOffSetY
-            }px`;
-        }
         }
 
         if (keycodes.LEFT[keyCode]) {
             if (shipInGaragePositionX[shipPickedIndex] > 0) {
                 shipInGaragePositionX[shipPickedIndex]--;
             }
-            shipsInGarage[shipPickedIndex].style.left = `${
-                shipInGaragePositionX[shipPickedIndex] * 60 + shipOffSetX
-            }px`;
-            shipsInGarage[shipPickedIndex].style.top = `${
-                shipInGaragePositionY[shipPickedIndex] * 60 + shipOffSetY
-            }px`;
+            shipPlacingWithPx();
         }
 
         if (keycodes.RIGHT[keyCode]) {
@@ -176,58 +172,52 @@ document.addEventListener("keydown", ({ keyCode }) => {
                 if (shipInGaragePositionX[shipPickedIndex] < 9) {
                     shipInGaragePositionX[shipPickedIndex]++;
                 }
-                shipsInGarage[shipPickedIndex].style.left = `${
-                    shipInGaragePositionX[shipPickedIndex] * 60 + shipOffSetX
-                }px`;
-                shipsInGarage[shipPickedIndex].style.top = `${
-                    shipInGaragePositionY[shipPickedIndex] * 60 + shipOffSetY
-                }px`;
-            }
-            else{
-                if (shipInGaragePositionX[shipPickedIndex] < 10 - shipBxinShip[shipPickedIndex]) {
+                shipPlacingWithPx();
+            } else {
+                if (
+                    shipInGaragePositionX[shipPickedIndex] <
+                    10 - shipBxinShip[shipPickedIndex]
+                ) {
                     shipInGaragePositionX[shipPickedIndex]++;
                 }
-                shipsInGarage[shipPickedIndex].style.left = `${
-                    shipInGaragePositionX[shipPickedIndex] * 60 + shipOffSetX
-                }px`;
-                shipsInGarage[shipPickedIndex].style.top = `${
-                    shipInGaragePositionY[shipPickedIndex] * 60 + shipOffSetY
-                }px`;
+                shipPlacingWithPx();
             }
-            
         }
 
-        shipInGaragePosition[shipPickedIndex] =
-            shipInGaragePositionY[shipPickedIndex] * 10 +
-            shipInGaragePositionX[shipPickedIndex];
+        shipPositionCalc();
 
         freeSquareCheck();
     }
-    console.log(shipInGaragePosition[shipPickedIndex]);
 });
 
 
 rotateShipButton.onclick = () => {
-    shipInGaragePosition[shipPickedIndex] =
-            shipInGaragePositionY[shipPickedIndex] * 10 +
-            shipInGaragePositionX[shipPickedIndex];
-    if (shipBxinShip[shipPickedIndex]%2 == 0) {
-        shipsInGarage[shipPickedIndex].style.transformOrigin = "left";
-    }
-    if (rotateIndex === false) {
-        rotateIndex = true;
-        shipsInGarage[shipPickedIndex].style.rotate = "-90deg";
-        shipOffSetX = 681;
-        shipOffSetY = 31;
-        console.log(shipInGaragePosition[shipPickedIndex]);
-    }
-    else {
-        rotateIndex = false;
-        shipsInGarage[shipPickedIndex].style.rotate = "0deg";
-        shipOffSetX = 561;
-        shipOffSetY = 91;
-    }
-}
+        shipPositionCalc();
+        shipsInGarage[shipPickedIndex].style.transformOrigin = "30px 30px";
+        if (shipBxinShip[shipPickedIndex] % 2 == 0) {
+        }
+        if (rotateIndex === false) {
+            rotateIndex = true;
+            shipsInGarage[shipPickedIndex].style.rotate = "-90deg";
+
+            if (shipInGaragePositionX[shipPickedIndex] > 6) {
+                shipInGaragePositionX[shipPickedIndex] =
+                    10 - shipBxinShip[shipPickedIndex];
+            }
+
+        } else {
+            rotateIndex = false;
+            shipsInGarage[shipPickedIndex].style.rotate = "0deg";
+
+            if (shipInGaragePositionY[shipPickedIndex] > 6) {
+                shipInGaragePositionY[shipPickedIndex] =
+                    10 - shipBxinShip[shipPickedIndex];
+            }
+        }
+        shipPlacingWithPx();
+
+};
+
 
 saveShipButton.onclick = () => {
     if (shipFreeSquare === false) {
@@ -236,48 +226,89 @@ saveShipButton.onclick = () => {
             saveShipButton.classList.remove("saveShipAnimation");
         }, 1000);
     } else {
-        shipInGaragePosition[shipPickedIndex] =
-            shipInGaragePositionY[shipPickedIndex] * 10 +
-            shipInGaragePositionX[shipPickedIndex];
-
-        //save the position
-        let savePosition = shipInGaragePosition[shipPickedIndex];
-
-        for (let i = 0; i < shipBxinShip[shipPickedIndex]; i++) {
-            myAreaIndex[savePosition] = 1;
-            savePosition += 10;
-        }
+        shipPositionCalc();
 
         movingIndex = false;
+        let savePosition = shipInGaragePosition[shipPickedIndex];
 
-        if (shipInGaragePositionX[shipPickedIndex] == 0) {
-            savePosition = shipInGaragePosition[shipPickedIndex] - 10;
-            for (let i = 0; i < shipBxinShip[shipPickedIndex] + 2; i++) {
-                for (let j = 0; j < 2; j++) {
-                    shipSquareIndex[savePosition] = true;
-                    savePosition++;
-                }
-                savePosition += 8;
+        if (rotateIndex === false) {
+            //save the position
+            for (let i = 0; i < shipBxinShip[shipPickedIndex]; i++) {
+                myAreaIndex[savePosition] = 1;
+                savePosition += 10;
             }
-        } else if (shipInGaragePositionX[shipPickedIndex] == 9) {
-            savePosition = shipInGaragePosition[shipPickedIndex] - 11;
-            for (let i = 0; i < shipBxinShip[shipPickedIndex] + 2; i++) {
-                for (let j = 0; j < 2; j++) {
-                    shipSquareIndex[savePosition] = true;
-                    savePosition++;
+    
+    
+            if (shipInGaragePositionX[shipPickedIndex] == 0) {
+                savePosition = shipInGaragePosition[shipPickedIndex] - 10;
+                for (let i = 0; i < shipBxinShip[shipPickedIndex] + 2; i++) {
+                    for (let j = 0; j < 2; j++) {
+                        shipSquareIndex[savePosition] = true;
+                        savePosition++;
+                    }
+                    savePosition += 8;
                 }
-                savePosition += 8;
-            }
-        } else {
-            savePosition = shipInGaragePosition[shipPickedIndex] - 11;
-            for (let i = 0; i < shipBxinShip[shipPickedIndex] + 2; i++) {
-                for (let j = 0; j < 3; j++) {
-                    shipSquareIndex[savePosition] = true;
-                    savePosition++;
+            } else if (shipInGaragePositionX[shipPickedIndex] == 9) {
+                savePosition = shipInGaragePosition[shipPickedIndex] - 11;
+                for (let i = 0; i < shipBxinShip[shipPickedIndex] + 2; i++) {
+                    for (let j = 0; j < 2; j++) {
+                        shipSquareIndex[savePosition] = true;
+                        savePosition++;
+                    }
+                    savePosition += 8;
                 }
-                savePosition += 7;
+            } else {
+                savePosition = shipInGaragePosition[shipPickedIndex] - 11;
+                for (let i = 0; i < shipBxinShip[shipPickedIndex] + 2; i++) {
+                    for (let j = 0; j < 3; j++) {
+                        shipSquareIndex[savePosition] = true;
+                        savePosition++;
+                    }
+                    savePosition += 7;
+                }
             }
         }
+        else {
+            //save the position
+            for (let i = 0; i < shipBxinShip[shipPickedIndex]; i++) {
+                myAreaIndex[savePosition] = 1;
+                savePosition ++;
+            }
+
+            if (shipInGaragePositionX[shipPickedIndex] == 0) {
+                savePosition = shipInGaragePosition[shipPickedIndex] - 10;
+                for (let i = 0; i < 3; i++) {
+                    for (let j = 0; j < shipBxinShip[shipPickedIndex]+1; j++) {
+                        shipSquareIndex[savePosition] = true;
+                        savePosition++;
+                    }
+                    savePosition += 9-shipBxinShip[shipPickedIndex];
+                }
+            }
+            else if (shipInGaragePositionX[shipPickedIndex] == 10-shipBxinShip[shipPickedIndex]) {
+                savePosition = shipInGaragePosition[shipPickedIndex] - 11;
+                for (let i = 0; i < 3; i++) {
+                    for (let j = 0; j < shipBxinShip[shipPickedIndex]+1; j++) {
+                        shipSquareIndex[savePosition] = true;
+                        savePosition++;
+                    }
+                    savePosition += 9-shipBxinShip[shipPickedIndex];
+                }
+            }
+            else{
+                savePosition = shipInGaragePosition[shipPickedIndex] - 11;
+                for (let i = 0; i < 3; i++) {
+                    for (let j = 0; j < shipBxinShip[shipPickedIndex]+2; j++) {
+                        shipSquareIndex[savePosition] = true;
+                        savePosition++;
+                    }
+                    savePosition += 8-shipBxinShip[shipPickedIndex];
+                }
+            }
+            
+        }
+
+        
 
         for (let i = 0; i < 100; i++) {
             if (shipSquareIndex[i] === true) {
@@ -287,21 +318,21 @@ saveShipButton.onclick = () => {
 
         saveShipButton.style.backgroundColor = "white";
         buildGarageOverLap.style.zIndex = "0";
-        savedShipsCount ++;
         saveButtonOverLap.style.zIndex = "10";
+        rotateButtonOverLap.style.zIndex = "10";
 
-        if (savedShipsCount == shipsInGarage.length){
+        savedShipsCount++;
+
+        if (savedShipsCount == shipsInGarage.length) {
             buildShipContinue.style.display = "block";
         }
     }
 };
 
-
 buildShipContinue.onclick = () => {
     buildShip.style.animation = "slideBuildShip 1s forwards";
     game.style.animation = "slideGame 1s forwards";
-}
-
+};
 
 [...enemySquares].forEach((enemySquare) => {
     enemySquare.onclick = () => {
--- res/js/main.js
+++ res/js/main.js
@@ -3,15 +3,47 @@ const spinBxs = document.getElementsByClassName("spinBx");
 const startTutorial = document.getElementById("startTutorial");
 
 
+
+
+let scale = [1, 1];
+let InInterval;
+let OutInterval;
+
 playGameMenu.onmouseenter = () => {
-    spinBxs[0].style.display = "block";
+    InInterval = setInterval(() => {
+        scale[0] += 0.01;
+        playGameMenu.style.transform = `scale(${scale[0]})`;
+    }, 50);
 };
-startTutorial.onmouseenter = () => {
-    spinBxs[1].style.display = "block";
-};
+
 playGameMenu.onmouseleave = () => {
-    spinBxs[0].style.display = "none";
+    clearInterval(InInterval);
+    OutInterval = setInterval(() => {
+        scale[0] -= 0.01;
+        playGameMenu.style.transform = `scale(${scale[0]})`;
+
+        if (scale[0] <= 1) {
+            clearInterval(OutInterval);
+        }
+    },10);
 };
+
+startTutorial.onmouseenter = () => {
+    InInterval = setInterval(() => {
+        scale[1] += 0.01;
+        startTutorial.style.transform = `scale(${scale[1]})`;
+    }, 50);
+};
+
 startTutorial.onmouseleave = () => {
-    spinBxs[1].style.display = "none";
+    clearInterval(InInterval);
+    OutInterval = setInterval(() => {
+        scale[1] -= 0.01;
+        startTutorial.style.transform = `scale(${scale[1]})`;
+
+        if (scale[1] <= 1) {
+            clearInterval(OutInterval);
+            console.log("ahoj");
+        }
+    },10);
 };
